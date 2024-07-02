98917b368ed37aa47d4908188c2d8de00047d032
Bikash Shrestha <stha.bikam99@gmail.com>
2024-07-02 23:45:52 +0545

    Clean Architecture

---
+++ ClothingStoreInfrastrucutre/Data/ClothDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ClothingStoreDomain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothingStoreInfrastrucutre.Data
+{
+    public class ClothDbContext: IdentityDbContext
+    {
+        public ClothDbContext(DbContextOptions<ClothDbContext> options): base(options)
+        {
+            
+        }
+        public DbSet<Cloth> Clothes { get; set; }
+    }
+}
